08b7dc596f66e8fef57d5bc070f2de26a0a918ce
rodrigobecher <rodrigofbecher@gmail.com>
2020-05-21 12:16:17 -0300

    first commit

---
+++ src/main/java/com/supero/taskssuperows/TasksSuperoWsApplication.java
@@ -0,0 +1,13 @@
+package com.supero.taskssuperows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksSuperoWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksSuperoWsApplication.class, args);
+	}
+
+}
